a7f2c9c7a234e1e4ba2e03894e27a0d518c0bd57
Ubuntu <saheedfaniran@gmail.com>
2023-03-19 08:33:18 +0000

    Adjusted terraform files 3

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-aws-tf-state-bucket"
     key = "main"
-g   region = "us-east-1"
+    region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
   }
 }
